7d88d6570e8719c4ac67700ccb8d692072e5cf77
jkr442 <jkr442@gmail.com>
2023-05-12 23:24:55 +1000

    sitewide update to navbar to include responsive menu and hamburger toggle

--- testnavbar.js
+++
@@ -1,8 +0,0 @@
-window.onload = function() {
-  const toggleButton = document.getElementById('check');
-  const navbarLinks = document.getElementsByClassName('navbar-links')[0];
-
-  toggleButton.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active');
-  });
-}
